49ee169b51b09b9c39b2458a3372248b7faa137b
AlejG2005 <droiiipm@hotmail.com>
2026-04-08 18:27:51 -0600

    adsfads

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Body } from '@nestjs/common';
-import { HttpCode } from '@nestjs/common';  
+import { HttpCode } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UseGuards, Get, Req } from '@nestjs/common';
 import { SupabaseAuthGuard } from './guards/supabase-auth.guard';
@@ -11,16 +11,16 @@ import { SupabaseAuthGuard } from './guards/supabase-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('login')
-  @HttpCode(200) 
   login(@Body() loginDto: LoginDto) {
+    console.log("BODY:", loginDto);
     return this.authService.login(loginDto);
   }
 
   @Post('register')
-  @HttpCode(200) 
+  @HttpCode(200)
   register(@Body() regisDto: RegisterDto) {
     return this.authService.register(regisDto);
   }
